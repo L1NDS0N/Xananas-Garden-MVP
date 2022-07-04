ba48dae9ad33394b7cf5eeef71cac6adc90a3381
Lindson França <l1nds0n@hotmail.com>
2022-07-04 17:22:30 -0300

    #3 template header iniciado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { MagnifyingGlass } from 'phosphor-react';
+import React from 'react';
+
+import Logo from '../../assets/Logo';
+
+const Header: React.FC = () => {
+  return (
+    <div
+      className="flex flex-1 flex-row border-solid items-center justify-around
+     border-b border-zinc-300 bg-zinc-100 h-20 gap-6"
+    >
+      <div className="w-full flex justify-end mx-2">
+        <div className="flex flex-row w-96 h-12 rounded">
+          <MagnifyingGlass color="gray" className="self-center" size={32} />
+          <input
+            type="text"
+            className="w-full text-center rounded"
+            placeholder="Buscar produto"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-row w-full overflow-hidden">
+        <div className="max-h-10">
+          <Logo />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
