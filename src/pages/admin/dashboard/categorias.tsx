446c25355d6820001c5be4d5f22068bbb68e50bd
Lindson França <l1nds0n@hotmail.com>
2022-07-12 16:55:19 -0300

    path refactoring

--- src/pages/admin/dashboard/categorias.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const categorias: React.FC = () => {
-  return <div>Cadastrar categorias</div>;
-}
-
-export default categorias;
--- src/pages/admin/dashboard/produtos.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const produtos: React.FC = () => {
-  return <div>Cadastrar produtos</div>;
-};
-
-export default produtos;
