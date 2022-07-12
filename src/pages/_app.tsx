4645e7ddb35cb8155fc281172f34e5a9ea19b3bf
Lindson França <l1nds0n@hotmail.com>
2022-07-12 16:54:53 -0300

    #14 render using nested layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout = NextPage &
+  AppProps & {
+    getLayout?: (page: any) => any;
+  };
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? (page => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp;
