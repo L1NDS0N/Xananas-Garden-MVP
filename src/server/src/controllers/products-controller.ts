23037b0519adfb0c293fd990b999c277d697d34d
Lindson França <l1nds0n@hotmail.com>
2022-07-14 16:57:47 -0300

    products' class implementation controls the routes

---
+++ src/server/src/controllers/products-controller.ts
@@ -0,0 +1,77 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaProductsRepository } from '../repositories/prisma/prisma-products-repository';
+import { ProductsPublishData } from '../repositories/products-repository';
+import { ProductsService } from '../services/products-service';
+
+export class ProductsController {
+  async postProduct(request: NextApiRequest, response: NextApiResponse) {
+    const productsRepository = new PrismaProductsRepository();
+    const productsService = new ProductsService(productsRepository);
+
+    const { body: data } = request as { body: ProductsPublishData };
+    
+    try {
+      await productsService.create(data);
+
+      return response.status(201).end();
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+
+  async getProduct(request: NextApiRequest, response: NextApiResponse) {
+    const productsRepository = new PrismaProductsRepository();
+    const productsService = new ProductsService(productsRepository);
+
+    const { id } = request.query as { id: string };
+
+    try {
+      const product = await productsService.findOne(id);
+      return response.status(200).json(product);
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+
+  async putProduct(request: NextApiRequest, response: NextApiResponse) {
+    const productsRepository = new PrismaProductsRepository();
+    const productsService = new ProductsService(productsRepository);
+
+    const { id } = request.query as { id: string };
+    const { body: data } = request as { body: ProductsPublishData };
+
+    try {
+      await productsService.updateOne(id, data);
+
+      return response.status(200).end();
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+
+  async deleteProduct(request: NextApiRequest, response: NextApiResponse) {
+    const productsRepository = new PrismaProductsRepository();
+    const productsService = new ProductsService(productsRepository);
+
+    const { id } = request.query as { id: string };
+
+    try {
+      await productsService.deleteOne(id);
+      return response.status(200).end();
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+
+  async getProducts(request: NextApiRequest, response: NextApiResponse) {
+    const productsRepository = new PrismaProductsRepository();
+    const productsService = new ProductsService(productsRepository);
+
+    try {
+      const products = await productsService.findAll();
+      return response.status(200).json(products);
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+}
